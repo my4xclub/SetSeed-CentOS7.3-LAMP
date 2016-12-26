ee17f84d8c8d7590c005911aeb3bc692d77ebaee
Allison Hensgen <sales@my4x.club>
2016-12-26 03:59:23 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -29,7 +29,7 @@ yum-config-manager --enable remi-php56 -y >> $LOG 2>&1
 cat >>/etc/yum.repos.d/MariaDB.repo<<EOF
 [mariadb]
 name = MariaDB
-baseurl = http://yum.mariadb.org/10.1/centos7-amd64
+baseurl = http://yum.mariadb.org/10.2.3/centos7-amd64
 gpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB
 gpgcheck=1
 EOF
